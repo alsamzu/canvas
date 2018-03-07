4a0182c920a2f19c7980d9d970e051cecb5a96ed
alsamzu <zulfalsam@gmail.com>
2018-03-07 21:13:55 +0300

    circles

--- js/custom.js
+++ js/custom.js
@@ -3,18 +3,18 @@ var canvas= document.querySelector('canvas');
 canvas.width=window.innerWidth;
 canvas.height=window.innerHeight;
 var c=canvas.getContext('2d');
-c.fillStyle= "red";
-c.fillRect(100,100,100,100);
-
-
-console.log(canvas);
-
-//drawing a line
-c.beginPath();
-c.moveTo(80,300);
-c.lineTo(500,80);
-c.strokeStyle="blue";
-c.stroke();
+//c.fillStyle= "red";
+//c.fillRect(100,100,100,100);
+//
+//
+//console.log(canvas);
+//
+////drawing a line
+//c.beginPath();
+//c.moveTo(80,300);
+//c.lineTo(500,80);
+//c.strokeStyle="blue";
+//c.stroke();
 
 //Arc //Circle
 c.beginPath();
