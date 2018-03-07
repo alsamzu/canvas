78f5911ec52a895089cc4cbfdd0ad186921df8ef
alsamzu <zulfalsam@gmail.com>
2018-03-07 08:22:10 +0300

    colors added

--- js/custom.js
+++ js/custom.js
@@ -3,12 +3,15 @@ var canvas= document.querySelector('canvas');
 canvas.width=window.innerWidth;
 canvas.height=window.innerHeight;
 var c=canvas.getContext('2d');
+c.fillStyle= "red";
 c.fillRect(100,100,100,100);
+
 
 console.log(canvas);
 
 //drawing a line
 c.beginPath();
-c.moveTo(60,300);
-c.lineTo(500,60);
+c.moveTo(80,300);
+c.lineTo(500,80);
+c.strokeStyle="blue";
 c.stroke();
